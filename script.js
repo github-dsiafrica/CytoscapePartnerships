cfe7541a2497d02602037a20881bdbd1a69ea465
Tino Muzambi <tino.muzambi@uct.ac.za>
2023-04-05 12:16:52 +0200

    Adds cytoscape network.

--- script.js
+++ script.js
@@ -1,3 +1,47 @@
 import cytoscape from "cytoscape";
 
-console.log("hi");
+const cy = cytoscape({
+	container: document.getElementById("cy"),
+	elements: [
+		// list of graph elements to start with
+		{
+			// node a
+			data: { id: "a" },
+		},
+		{
+			// node b
+			data: { id: "b" },
+		},
+		{
+			// edge ab
+			data: { id: "ab", source: "a", target: "b" },
+		},
+	],
+
+	style: [
+		// the stylesheet for the graph
+		{
+			selector: "node",
+			style: {
+				"background-color": "#ff0000",
+				label: "data(id)",
+			},
+		},
+
+		{
+			selector: "edge",
+			style: {
+				width: 3,
+				"line-color": "#ccc",
+				"target-arrow-color": "#ccc",
+				"target-arrow-shape": "triangle",
+				"curve-style": "bezier",
+			},
+		},
+	],
+
+	layout: {
+		name: "grid",
+		rows: 1,
+	},
+});
