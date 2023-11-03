4d472220a0e1dbb1c2a51be3d7687414d47f7767
Tino <tino.muzambi@uct.ac.za>
2023-11-03 22:02:11 +0200

    Adds projects map.

---
+++ utils/projectsMap.js
@@ -0,0 +1,125 @@
+export const dataMap = new Map([
+	[
+		"a",
+		"Advancing discovery for developmental disorders - expanded analysis of the DDD-Africa resource (DDD-Africa)",
+	],
+	[
+		"b",
+		"Advancing Public Health Research in Eastern Africa through Data Science Training (APHREA-DST)",
+	],
+	[
+		"c",
+		"Application of Data Science to Build Research Capacity in Zoonoses and Foodborne Infections in West Africa (DS-ZOOFOOD)",
+	],
+	[
+		"d",
+		"Artificial Intelligence assisted echocardiography to facilitate optimal image extraction for congenital heart defects diagnosis in Sub-Saharan Africa",
+	],
+	[
+		"e",
+		"Automated Mobile Microscopy for Malaria Diagnosis and surveillance in Uganda",
+	],
+	[
+		"f",
+		"BCX-Africa: Utilizing data science to evaluate the applicability of blood cell traits polygenic risk scores for disease prediction in Africa",
+	],
+	[
+		"g",
+		"Bridging Gaps in the ELSI of Data Science Health Research in Nigeria (BridgELSI)",
+	],
+	[
+		"h",
+		"CHaracterizing Effects of Air Quality In Maternal, Newborn and Child Health: The CHEAQI-MNCH Research Project",
+	],
+	[
+		"i",
+		"Combatting AntiMicrobial Resistance in Africa Using Data Science (CAMRA)",
+	],
+	["j", "Computational Omics and Biomedical Informatics Program (COBIP)"],
+	[
+		"k",
+		"Data Science and Medical Image Analysis Training for Improved Health Care Delivery in Nigeria (DATICAN)",
+	],
+	["l", "Data Science for Child Health Now in Ghana (DS-CHANGE)"],
+	[
+		"m",
+		"Developing data science solutions to mitigate the health impacts of climate change in Africa: the HE2AT Center (HEAT)",
+	],
+	["n", "DS-I Africa - LAW"],
+	["o", "DS-I Africa Coordinating Center"],
+	[
+		"p",
+		"DSpace: Utilizing Data Science to Predict and Improve Health Outcomes in Pediatric HIV",
+	],
+	["q", "eLwazi (Open Data Science Platform)"],
+	[
+		"r",
+		"Eneza Data Science: Enhancing Data Science Capability and Tools for Health in East Africa",
+	],
+	[
+		"s",
+		"Genome-wide characterization of complex variants and their phenotypic effects in African populations",
+	],
+	[
+		"t",
+		"Geo-enabled detect and respond system for antimalarial resistance in Ghana: GDRS - Ghana",
+	],
+	[
+		"u",
+		"Growing Data-science Research in Africa to Stimulate Progress (GRASP)",
+	],
+	[
+		"v",
+		"Harnessing Data Science to Promote Equity in Injury and Surgery for Africa (D-SINE)",
+	],
+	["w", "INnovative data Science to Impact the TB Epidemic (INSITE)"],
+	["x", "Integrated modeLs for Early Risk-prediction in Africa (ILERA) study"],
+	[
+		"y",
+		"Leveraging artificial intelligence/machine learning-based technology to overcome specialized training and technology barriers for the diagnosis and prognostication of colorectal cancer in Africa",
+	],
+	[
+		"z",
+		"Leveraging Data Science Applications to Improve Children's Environmental Health in Sub-Saharan Africa (DICE Project)",
+	],
+	[
+		"aa",
+		"MADIVA (Multimorbidity in Africa: Digital innovation, visualization, and application)",
+	],
+	[
+		"ab",
+		"Makerere University Data Science Research Training to Strengthen Evidence-Based Health Innovation",
+	],
+	["ac", "MUST Data Science Research Hub (MUDSReH)"],
+	["ae", "NYU-Moi Data Science for Social Determinants Training Program"],
+	[
+		"af",
+		"Public Understanding of Big data in Genomics Medicine in Africa (PUBGEM-Africa)",
+	],
+	["ag", "Research for Ethical Data Science in Southern Africa (REDSSA)"],
+	["ah", "Research Training in Data Science for Health in Rwanda"],
+	[
+		"ai",
+		"Research Training on Harnessing Data Science for Global Health Priorities in Africa",
+	],
+	[
+		"aj",
+		"Role of Data Streams In Informing Infection Dynamics in Africa- INFORM Africa",
+	],
+	[
+		"ak",
+		"SYNthetic Healthcare DAta Platform for Data SciEnce Training ('SYNAPSE')",
+	],
+	[
+		"al",
+		"Tuberculosis in households with infectious cases in Kampala city: Harnessing health data science for new insights on TB transmission and treatment response (DS-IAFRICA-TB)",
+	],
+	[
+		"am",
+		"UZIMA-DS: UtiliZing health Information for Meaningful impact in East Africa through Data Science",
+	],
+	[
+		"an",
+		"West Africa Center of Excellence for Data Science Research Education",
+	],
+]);
